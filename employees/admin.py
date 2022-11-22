1cef878043eb0e1073c5673b2b8a960bd690846b
tetelevm <tetelev99@mail.ru>
2022-11-23 02:09:20 +0300

    Added required models

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db.models.fields import TextField
 
-# Register your models here.
+from .models import Department, Post, Employee
+
+
+class _FlatText(admin.ModelAdmin):
+    formfield_overrides = {
+        TextField: {"widget": Textarea(attrs={"rows": 1, "cols": 60})},
+    }
+
+
+@admin.register(Department)
+class DepartmentAdmin(_FlatText):
+    fields = (("name", "parent_department"),)
+
+
+@admin.register(Post)
+class PostAdmin(_FlatText):
+    fields = ("name",)
+
+
+@admin.register(Employee)
+class QuestionChoiceAdmin(_FlatText):
+    fields = (
+        ("name",),
+        ("date_employment", "is_active"),
+        ("department", "post", "salary"),
+    )
+
+
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,106 @@
-from django.db import models
+from django.db.models import Model, Index, Q, PROTECT
+from django.db.models import ForeignKey, TextField, DateField, BooleanField, IntegerField
+from django.core.validators import MinValueValidator
+from django.db.models.constraints import CheckConstraint
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+__all__ = [
+    "Department",
+    "Post",
+    "Employee",
+]
+
+
+_max_nesting_level = 5
+
+
+class Department(MPTTModel):
+    MAX_NESTING_LEVEL = _max_nesting_level
+
+    name = TextField(
+        verbose_name="Department name",
+        null=False,
+    )
+    parent_department = TreeForeignKey(
+        "self",
+        verbose_name="Parent department",
+        on_delete=PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check=Q(level__lte=_max_nesting_level),
+                name="not_very_nest"
+            ),
+        ]
+
+    class MPTTMeta:
+        order_insertion_by = ["-id"]
+        parent_attr = "parent_department"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(Model):
+    name = TextField(
+        verbose_name="Post name",
+        null=False,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(Model):
+    name = TextField(
+        verbose_name="Full name",
+        null=False,
+    )
+    date_employment = DateField(
+        verbose_name="Date of employment",
+        null=False,
+    )
+    post = ForeignKey(
+        "employees.Post",
+        verbose_name="Employee post",
+        on_delete=PROTECT
+    )
+    department = ForeignKey(
+        "employees.Department",
+        verbose_name="Department",
+        on_delete=PROTECT,
+        null=True,
+        blank=True,
+    )
+    salary = IntegerField(
+        verbose_name="Employee salary",
+        null=False,
+        default=0,
+        validators=[MinValueValidator(0)],
+    )
+    is_active = BooleanField(
+        verbose_name="Is employee working",
+        default=True,
+    )
+
+    class Meta:
+        indexes = [
+            Index(fields=["date_employment"]),
+            Index(fields=["is_active"]),
+        ]
+        constraints = [
+            CheckConstraint(
+                check=(
+                    (Q(is_active=True) & Q(department_id__isnull=False))
+                    | (Q(is_active=False) & Q(department_id__isnull=True))
+                ),
+                name="works_in_department"
+            ),
+        ]
+
